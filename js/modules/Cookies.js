253d9110b6e3bf6d152ac713b3b74e428a4d94d0
John Pork <John@Pork.pl>
2023-05-12 14:44:24 +0200

    initial commit

---
+++ js/modules/Cookies.js
@@ -0,0 +1,49 @@
+export class Cookies {
+    constructor() {
+        this.checkCookieEnabled();
+    }
+    checkCookieEnabled() {
+        if(!navigator.cookieEnabled){
+            alert('Masz wyłączoną obsługę ciasteczek');
+            return;
+        }
+    }
+    setCookie(options) {
+        const cookieSettingsTab = [];
+
+        cookieSettingsTab.push(`${options.name}=${options.value}`);
+
+        if(typeof options.days === 'number'){
+            const date =  new Date();
+            date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000));
+            cookieSettingsTab.push(`expires=${date.toGMTString()}`)
+        }
+
+        document.cookie = cookieSettingsTab.join(';')
+
+        console.log(cookieSettingsTab)
+    }
+    getCookie(name) {
+        if(document.cookie != '') {
+
+            const cookies = document.cookie.split(/; */);
+
+            for(let i=0; 1 < cookies.length; i++) {
+                const cookieName = cookies[i].split('=')[0];
+                const cookieVal = cookies[i].split('=')[1];
+
+                if(cookieName === name){
+                    return cookieVal;
+                }
+            }
+
+        }
+
+    }
+    removeCookie(name) {
+        this.setCookie({
+            name: name,
+            days: -1
+        })
+    }
+}
